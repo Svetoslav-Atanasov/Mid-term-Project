a897515daab3306f574ce7bc87c22d7bf74c32cb
Svetoslav <svetoslav_atanasov@mail.bg>
2019-02-11 21:33:06 +0200

    optimized functions code for login, create new account, updated userTemplate.html links and buttons layout

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,12 +3,24 @@ const byId = (id) => document.getElementById(id);
 
 userStorage.init();
 
-byId('logInButton').addEventListener('click', function(event) {
+byId('logInButton').addEventListener('click', function() {
+    loginUser(event);
+}, false);
+
+const ENTER = 13;
+document.body.addEventListener('keypress', function(event) {
+    if (event.keyCode === ENTER) {
+        loginUser(event);
+    }
+}, false);
+
+function loginUser(event) {
     event.preventDefault();
 
     const username = byId('usernameInput').value;
     const password = byId('passwordInput').value;
 
+    console.log('ENTER')
     const user = userStorage.login(username, password);
     // check the username, password input
     console.log(user)
@@ -19,26 +31,4 @@ byId('logInButton').addEventListener('click', function(event) {
         // finds the first element of that class and displays the error message
         document.querySelector('.errorLogin').style.display = 'block'
     }
-}, false);
-
-const ENTER = 13;
-document.body.addEventListener('keypress', function(event) {
-    if (event.keyCode === ENTER) {
-        event.preventDefault();
-        const username = byId('usernameInput').value;
-        const password = byId('passwordInput').value;
-
-        console.log('ENTER')
-        const user = userStorage.login(username, password);
-        // check the username, password input
-        console.log(user)
-            //checks userList for username, password and if the inputs are empty fields
-        if (user) {
-            document.location.href = '../html/userTemplate.html'
-        } else {
-            // finds the first element of that class and displays the error message
-            document.querySelector('.errorLogin').style.display = 'block'
-        }
-    }
-
-}, false);
+}
